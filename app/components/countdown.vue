fe0a4c0baac24d54982a687503258f3e303298b1
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-01-11 19:05:15 -0500

    feat: home page

---
+++ app/components/countdown.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>[BIG TIMER]</div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,42 @@
+<script setup lang="ts">
+// const { data: home } = await useAsyncData(() =>
+//   queryCollection("content").path("/").first()
+// );
+</script>
+
 <template>
-  <main>
-    <ImageGallery />
-    <RedirectsPanel />
-    <MessagesPanel />
-  </main>
+  <UPage>
+    <UPageHero
+      headline="2025 (not so) Annual"
+      title="BennyHacks"
+      description="Hackathon @ CCNY"
+      orientation="horizontal"
+      :links="[
+        {
+          label: 'Pre-register',
+          to: '/register',
+          icon: 'i-lucide-milestone',
+        },
+        {
+          label: 'Learn More',
+          color: 'neutral',
+          variant: 'subtle',
+          trailingIcon: 'i-lucide-chevrons-down',
+          to: '#when',
+        },
+      ]"
+    >
+      <UPageCard highlight class="w-full place-self-center aspect-video">
+        pretty thematic image
+      </UPageCard>
+    </UPageHero>
+
+    <UPageBody>
+      <When />
+      <Where />
+      <Faq />
+      <Sponsors />
+      <Prizes />
+    </UPageBody>
+  </UPage>
 </template>
